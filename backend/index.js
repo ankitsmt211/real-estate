600d97701e55de1e1130a2c40f39501667d63341
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-02-20 15:24:05 +0530

    keep all property data in one schema and add function for db connectivity

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 
+const mongoose = require('mongoose')
+
 const app = express()
+
+app.use(express.json())
+
 const PORT = 8080
 
 app.get('/hello-server',(req,res)=>{
     res.send("hello from server")
 })
+
+
+
+
+mongoose.connect('mongodb://localhost/testreal').then(successful=>{
+    console.log("connected to db")
+}).catch(err=>
+    console.log("failed connection",err)
+)
+
 
 
 app.listen(PORT, ()=>{
