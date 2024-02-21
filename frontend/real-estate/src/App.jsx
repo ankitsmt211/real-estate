f49a386c0d5ceba14802044d7e3b6e1f09b9e51d
unknown <33ayushchauhan@gmail.com>
2024-02-21 20:42:01 +0530

    added sidebar and navbar

--- frontend/real-estate/src/App.jsx
+++ frontend/real-estate/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
-import Base from './Components/Base/Base.jsx'
+import Base from './Components/Base/Base.jsx';
+import NavBar from './Components/NavBar';
+import Sidebar from './Components/Sidebar';
+
 
 function App() {
-  return <>
-  <Base />
-  </>
+  return <div className='main'>
+  <Sidebar/>
+  <div className='eleArea'><NavBar/><Base /></div>
+  
+  </div>
 }
 export default App
