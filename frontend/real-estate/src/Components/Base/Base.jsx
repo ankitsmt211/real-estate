f5b3e365e119c97183183043f9150e6d11c9954e
alphaBEE <61616007+ankitsmt211@users.noreply.github.com>
2024-02-22 17:56:28 +0530

    routes and user login/Register * modify Base component to include all routes     * sets auth context(authenticated user) for all of application     * renders component based on route * add login component * add register component * styling for login/register

--- frontend/real-estate/src/Components/Base/Base.jsx
+++ frontend/real-estate/src/Components/Base/Base.jsx
@@ -1,5 +1,33 @@
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom"
+import Sidebar from "../Sidebar/Sidebar"
+import TopBar from "../Topbar/TopBar"
+import Login from "../Auth/Login"
+import Register from "../Auth/Register"
+import { useContext } from 'react';
+import { authContext } from '../../App';
+import '../Base/base.css'
+
 export default function Base(){
+    const { isLoggedIn } = useContext(authContext);
+
     return <>
-    <h1>Hello From React</h1>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/home" element={isLoggedIn ? <DashBoard /> : <Navigate to={'/login'} />}/>
+            <Route path="/login" element={<Login/>}/>
+            <Route path="/register" element={<Register/>}/>
+        </Routes>
+    </BrowserRouter>
+    </>
+}
+
+function DashBoard(){
+    return <>
+     <div className="main">
+    <Sidebar/>
+      <div className='eleArea'>
+        <TopBar/>
+      </div>
+    </div>
     </>
 }
